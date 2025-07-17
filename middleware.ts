90a26e9e52aad38dc3da41f64257409dfd962c72
Fereshteh Namazi <fereshte.namazi.edu@gmail.com>
2025-07-17 10:58:53 +0330

    Update - middleare api call

--- middleware.ts
+++ middleware.ts
@@ -1,15 +1,13 @@
-import { GETGenres } from "./api/genre/route";
-import { Genre } from "./types";
 import { NextResponse, type NextRequest } from "next/server";
 
 export async function middleware(request: NextRequest) {
   const response = NextResponse.next();
 
-  const genres: Genre[] = await GETGenres();
-  const genresList = request.cookies.get("genre");
+  // const genres: Genre[] = await GETGenres();
+  // const genresList = request.cookies.get("genre");
 
-  if (!genresList) {
-    response.cookies.set("genre", JSON.stringify(genres));
-  }
+  // if (!genresList) {
+  //   response.cookies.set("genre", JSON.stringify(genres));
+  // }
   return response;
 }
